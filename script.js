80dd8eca8afa57604515c50590676294ccc76eb7
Gunjesh Basnet <gunjesh.basnet550208@gmail.com>
2023-12-03 22:20:55 +0545

    ui update

--- script.js
+++ script.js
@@ -1,38 +1,60 @@
-var timer;
-var timeLeft;
-var isTimerRunning = false;
+let timer;
+let isPaused = false;
+let remainingTime = 1500; // Default Pomodoro time
+let duration = 1500; // Initial duration
 
-function startTimer() {
-  if (!isTimerRunning) {
-    var minutes = 25;
-    timeLeft = minutes * 60;
-    isTimerRunning = true;
-
-    timer = setInterval(function () {
-      var minutesDisplay = Math.floor(timeLeft / 60);
-      var secondsDisplay = (timeLeft % 60).toString().padStart(2, "0");
-
-      document.getElementById("timer").innerHTML = minutesDisplay + ":" + secondsDisplay;
-
-      if (timeLeft <= 0) {
-        clearInterval(timer);
-        document.getElementById("timer").innerHTML = "00:00";
-        isTimerRunning = false;
-        alert("Pomodoro session completed! Take a break.");
-      }
-
-      timeLeft--;
-    }, 1000);
+function startPauseTimer() {
+  if (timer) {
+    pauseResumeTimer();
+  } else {
+    startTimer();
+    updateButton("Pause");
   }
 }
 
-function pauseTimer() {
-  clearInterval(timer);
-  isTimerRunning = false;
+function setDuration(newDuration) {
+  duration = newDuration;
+  displayTimeLeft(duration);
 }
 
-function resetTimer() {
+function startTimer() {
   clearInterval(timer);
-  isTimerRunning = false;
-  document.getElementById("timer").innerHTML = "25:00";
+  isPaused = false;
+  displayTimeLeft(duration);
+  timer = setInterval(() => {
+    remainingTime = duration;
+    duration--;
+    if (duration < 0) {
+      clearInterval(timer);
+      alert("Time's up!");
+      timer = null;
+      updateButton("Start");
+    } else {
+      displayTimeLeft(duration);
+    }
+  }, 1000);
+}
+
+function pauseResumeTimer() {
+  if (isPaused) {
+    isPaused = false;
+    updateButton("Pause");
+    startTimer();
+  } else {
+    isPaused = true;
+    clearInterval(timer);
+    updateButton("Start");
+  }
+}
+
+function updateButton(action) {
+  const timerButton = document.getElementById("timerButton");
+  timerButton.textContent = action;
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes}:${remainderSeconds < 10 ? "0" : ""}${remainderSeconds}`;
+  document.getElementById("timer").textContent = display;
 }
